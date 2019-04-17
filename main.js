f164cf0e60811dc1e7c6f64165cc1f28901410de
Agnieszka <niemiec.agnieszka@interia.pl>
2019-04-17 21:58:23 +0200

    changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("visibilitychange", function() {
     if (document.hidden) {
        var title = document.getElementById('title');
-       title.innerHTML = 'Come back';
+       title.innerHTML = 'Hey, Come back! 🤗';
     } else {
        var title = document.getElementById('title');
        title.innerHTML = 'Medically';
